0c3985fba043d4fbc8335b34f5173274fc056291
mateuszbugaj <mateuszbugaj5@gmail.com>
2020-12-04 19:47:13 +0100

    basic pos rot frame rgb est

--- dataset_generator/src/main/java/View/Shape.java
+++ dataset_generator/src/main/java/View/Shape.java
@@ -47,7 +47,7 @@ public class Shape extends ProcessingElement {
 
     public void randomizePos(){
         int z = (int) p.random(20, 40);
-        int upperBound = (int) PApplet.map(z, 20, 40, 25, 12);
+        int upperBound = (int) PApplet.map(z, 20, 40, 17, 6);
         int x = (int) p.random(-upperBound, upperBound);
         int y = (int) p.random(-upperBound, upperBound);
 
